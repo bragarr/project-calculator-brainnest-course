aa7f0c10eaec9eb8ca7c222e81edde4b9ab268af
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-09 12:34:16 -0400

    Style and js supdated

--- calculator.js
+++ calculator.js
@@ -1,13 +1,12 @@
 let buttonOperation = document.querySelectorAll(".calc__buttons");
-
 let allClear = document.querySelector(".all__clear");
 let deleteButton = document.querySelector(".delete__button");
 let resultButton = document.querySelector(".result__button");
 let userEntryResult = document.querySelector(".user__entry");
-let calcOperationInput = document.querySelector(".result");
-let decimalAllowed = false;
 let changeMath = document.querySelector(".change__math");
 let restartUserInput = false;
+let alreadyChoseOperation = false;
+let alreadyChoseDote = false;
 const mathOp = ["+","-","/","*"]
 
 // buttonOperation Listener to turn on buttons
@@ -15,18 +14,8 @@ buttonOperation.forEach(button => {
     button.addEventListener("click", () => {
         let buttonSelectedByUser = button.dataset.value;
         addUserEntryResultButtonSelected(buttonSelectedByUser);
-        switch(buttonSelectedByUser) {
-            case "+":
-            case "/":
-            case "*":
-            case "-":    
-                if(calcOperationInput.value === "") {
-                    calcOperationInput.value = userEntryResult.value;
-                    userEntryResult.value = userEntryResult.value.slice(0, -1);
-                    restartUserInput = true;
-                } else {
-                    calcOperationInput.value += userEntryResult.value.slice(0, -1);
-                }
+        if(buttonSelectedByUser === ".") {
+            alreadyChoseDote = true;
         }
     })
 })
@@ -40,6 +29,9 @@ deleteButton.addEventListener("click", () => deleteLastofUserEntryResult());
 // Change math listener to change between positive and negative number
 changeMath.addEventListener("click", () => changeMathPositiveNegativeNumber());
 
+// Operates Calculator Math chosen by user
+resultButton.addEventListener("click", () => operate());
+
 // This function add numbers and operatos on calculator screen
 function addUserEntryResultButtonSelected(numberOperation) {
     let lastValueOnUserEntryResult = userEntryResult.value[userEntryResult.value.length-1]
@@ -48,25 +40,32 @@ function addUserEntryResultButtonSelected(numberOperation) {
     }
     if(userEntryResult.value.length===0 && numberOperation === ".") {
         userEntryResult.value = "0.";
-        decimalAllowed = true;
         return;
     }
     if(userEntryResult.value.length===0 && !Number(numberOperation)) { // Prevent Symbol on start
         return;
     }
-
-    if(calcOperationInput.value != "" && restartUserInput) {
-        userEntryResult.value = ""
+    if(restartUserInput && Number(numberOperation)) {  //After complete a operation, if user select a new number, a new operation starts
+        reloadCalculator();
         restartUserInput = false;
     }
-    
+    if(alreadyChoseDote && numberOperation === ".") {
+        return;
+    }
+    if(mathOp.includes(numberOperation)) {
+        alreadyChoseOperation = true;
+        alreadyChoseDote = false;
+    }
+    if(alreadyChoseOperation && mathOp.includes(numberOperation)) {
+        operate();
+        alreadyChoseOperation = false;
+    }
     userEntryResult.value += numberOperation;
-    
+    restartUserInput = false; 
 }
 
 function reloadCalculator() {
     userEntryResult.value = "";
-    calcOperationInput.value = "";
 }
 
 // This function delete the last element of calculator screen
@@ -81,44 +80,35 @@ function changeMathPositiveNegativeNumber() {
     }
 }
 
-function mathOperation() {
-    for(let i=0; i < calcOperationInput.value.length; i++) {
-        if(mathOp.includes(calcOperationInput.value[i])) {
-            switch(calcOperationInput.value[i]) {
+function operate() {
+    for(let i=0; i < userEntryResult.value.length; i++) {
+        if(mathOp.includes(userEntryResult.value[i])) {
+            switch(userEntryResult.value[i]) {
                 case "+":
-                    let number1 = calcOperationInput.value.slice(0,i)*1
-                    let number2 = calcOperationInput.value.slice(i+1,calcOperationInput.value.length)*1
-                    let soma = number1 + number2;
-                    calcOperationInput.value = "";
-                    userEntryResult.value = soma;
-    
-                case "*":
-                    let number3 = calcOperationInput.value.slice(0,i)*1
-                    let number4 = calcOperationInput.value.slice(i+1,calcOperationInput.value.length)*1
-                    let mult = number3*number4;
-                    calcOperationInput.value = "";
-                    userEntryResult.value = mult;
-                case "/":
-                    let number5 = calcOperationInput.value.slice(0,i)*1
-                    let number6 = calcOperationInput.value.slice(i+1,calcOperationInput.value.length)*1
-                    let div = number5/number6;
-                    calcOperationInput.value = "";
-                    userEntryResult.value = div;
+                    let sum = userEntryResult.value.slice(0,i)*1 + 
+                    userEntryResult.value.slice(i+1, userEntryResult.value.length)*1
+                    userEntryResult.value = sum;
+                    break;
                 case "-":
-                    let number7 = calcOperationInput.value.slice(0,i)*1
-                    let number8 = calcOperationInput.value.slice(i+1,calcOperationInput.value.length)*1
-                    let sub = number7-number8;
-                    calcOperationInput.value = "";
-                    userEntryResult.value = sub;
+                    let minus = userEntryResult.value.slice(0,i)*1 - 
+                    userEntryResult.value.slice(i+1, userEntryResult.value.length)*1
+                    userEntryResult.value = minus;
+                    break;
+                case "/":
+                    let div = userEntryResult.value.slice(0,i)*1 / 
+                    userEntryResult.value.slice(i+1, userEntryResult.value.length)*1
+                    userEntryResult.value = div;
+                    break;    
+                case "*":
+                    let times = userEntryResult.value.slice(0,i)*1 * 
+                    userEntryResult.value.slice(i+1, userEntryResult.value.length)*1
+                    userEntryResult.value = times;
+                    break;        
             }
         }
-    }
-}
+        if(userEntryResult.value[0]=== "-") {
 
-resultButton.addEventListener("click", () => {
-    if(calcOperationInput.value != "") {
-        calcOperationInput.value += userEntryResult.value;
-        userEntryResult.value = "";
-        mathOperation();
-    }    
-})
+        }
+    }
+    restartUserInput = true;
+}
